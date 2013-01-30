c00c06aa85dea78b6142245dead5799242496202
Niklas Bergius <niklas@bergius.org>
2013-01-30 11:04:59 +0100

    ...

--- RpnCalculator.Logic/Script.fsx
+++ RpnCalculator.Logic/Script.fsx
@@ -1,6 +1,7 @@
 ﻿// Learn more about F# at http://fsharp.net. See the 'F# Tutorial' project
 // for more guidance on F# programming.
 
+#r "bin\\Debug\\RpnCalculator.Collections.dll"
 #load "Calculator.fs"
 open RpnCalculator.Logic
 
